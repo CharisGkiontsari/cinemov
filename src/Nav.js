11f55a0fa2665e46cfd67087ec61288b6d68f3e5
HarisGkn <haris.gjo@gmail.com>
2021-04-24 14:13:35 +0300

    initialized react app, added nav, created logo and picked a color pallette(needs work)

---
+++ src/Nav.js
@@ -0,0 +1,27 @@
+
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+import logo from './logo.png';
+import SearchIcon from '@material-ui/icons/Search'
+import { Link } from 'react-router-dom'
+
+function Nav() {
+
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <img className="nav__logo" src={logo} alt="Logo" />
+            </Link>
+            <div className="nav__search">
+                <input type="text" className="nav__searchInput" />
+                <SearchIcon className="nav__searchIcon" />
+            </div>
+        </nav>
+
+
+
+    )
+}
+
+export default Nav
+
